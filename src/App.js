ce687afa8c9cad3ab5325fb340f4529e20c9ae10
Shivang Garg <shivang10garg@gmail.com>
2022-01-15 22:40:20 +0530

    fixes github workflow actions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                React Docker testing app
+                React Docker testing app!
             </header>
         </div>
     );
