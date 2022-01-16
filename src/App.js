6e04b93a420f7d0e92ff5c6498feb5d0daded695
Shivang Garg <shivang10garg@gmail.com>
2022-01-16 20:27:24 +0530

    updates dockerfile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
             <header className="App-header">
                 React Docker testing app!
                 Testing github workflow actions
-                adds docker file with update
+                adds docker file with update!
             </header>
         </div>
     );
