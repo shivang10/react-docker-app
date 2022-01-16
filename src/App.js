88f738186cd5dbab813e51e44b7a4cae32db0af4
Shivang Garg <shivang10garg@gmail.com>
2022-01-16 12:27:18 +0530

    🔧 updates github workflow actions

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
             <header className="App-header">
                 React Docker testing app!
                 Testing github workflow actions
+                without branches
             </header>
         </div>
     );
